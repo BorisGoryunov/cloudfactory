41f3731a0b14ca8d6cfa641206efb8266c0f697a
Boris Goryunov <rathome@narod.ru>
2025-09-09 13:47:26 +0700

    1m

--- tests/batched-ids-test.js
+++ tests/batched-ids-test.js
@@ -7,8 +7,17 @@ const IDS_COUNT = 1000;            // Сколько уникальных ID
 const BATCH_SIZE = TOTAL_REQUESTS / IDS_COUNT; // Запросов на один ID
 
 export const options = {
-  vus: 100,         
-  duration: '60s', 
+   vus: 100,         
+   duration: '1m', 
+    // stages: [
+    //     { duration: "15s", target: 20 },
+    //     { duration: "15s", target: 50 },
+    //     { duration: "35s", target: 150 },
+    //     { duration: "1m", target: 250 },
+    //     { duration: "15s", target: 50 },
+    //     { duration: "10s", target: 20 },
+    //     { duration: "2s", target: 0 }
+    //   ],
   thresholds: {
     http_req_duration: ['p(95) < 500'],
     http_req_failed: ['rate < 0.01'],
@@ -19,6 +28,8 @@ export const options = {
 export default function () {
 
   const myId = (__VU % IDS_COUNT) + 1;
+
+  
 
   for (let i = 0; i < BATCH_SIZE; i++) {
     
